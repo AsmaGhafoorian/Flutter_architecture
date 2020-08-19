bd37f99a636c58e2efe26a4fbf94c4561691bc5f
Asma Ghafourian <a.ghafourian@asiatech.ir>
2020-08-19 16:39:14 +0430

    added check box

--- lib/src/ui/home/profile.dart
+++ lib/src/ui/home/profile.dart
@@ -2,14 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test_app/src/bloc/MovieBloc.dart';
-import 'package:flutter_test_app/src/bloc/movie_detail_bloc_provider.dart';
-import 'package:flutter_test_app/src/model/movie_model.dart';
-
+import 'package:flutter_test_app/src/utils/text_styles.dart';
 import 'package:inject/inject.dart';
-
-import '../../bloc/MovieBloc.dart';
-
 
 @provide
 class Profile extends StatefulWidget{
@@ -32,6 +26,15 @@ class _ProfileState extends State<Profile>{
   var currentSelectedValue;
   final _gender = ["male", "female"];
   bool _isSwitch = false;
+  bool _isCheckboxSelected = false;
+  Map<String, bool> _checkBoxValues = {
+    "Writing" : true,
+    "Cooking" : false,
+    "Gardening": false,
+    "Cycling": false,
+    "Photography": false,
+    "Cricket": false
+  };
   @override
   Widget build(BuildContext context) {
 
@@ -39,32 +42,48 @@ class _ProfileState extends State<Profile>{
     return(Material(
         type: MaterialType.transparency, // remove yellow text underline
 
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: ListView(
+        children: <Widget>[ Container(
 
             child: Form(
               key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children :<Widget>[
-                      Text(
-                        'About You',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.blueAccent,
-                            fontSize: 20,
-                          fontWeight: FontWeight.bold
-                        ),
+
+                  Center(
+                    child: Text(
+                      'About You',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                          fontSize: 20,
+                        fontWeight: FontWeight.bold
                       ),
-                    ],
+                    ),
                   ),
 
+
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child:Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+
+                      children: [
+                        Text(
+                          'Contatct Details',
+                            style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ]
+                    ),
+                  ),
                   Padding(
 
-                    padding: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                     child: TextFormField(// for validation and allows to show error with fail validation
                         validator: (value){
                           if(value.isEmpty){
@@ -72,13 +91,14 @@ class _ProfileState extends State<Profile>{
                           }
                           return null;
                         },
-                        decoration: InputDecoration(
+                        decoration: InputDecoration(//The border, labels, icons, and styles used to decorate a Material Design text field
+                            contentPadding: EdgeInsets.symmetric(horizontal: 15),
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(5.0)
                             ),
                           hintText: 'Name',
                           hintStyle: TextStyle(
-                            color: Colors.black,
+                            color: Colors.grey,
                             fontWeight: FontWeight.bold,
                             fontSize: 12,
                           )
@@ -87,7 +107,7 @@ class _ProfileState extends State<Profile>{
                   ),
 
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                       child: TextFormField(   // for validation and allows to show error with fail validation
                         validator: (value){
                           if(value.isEmpty){
@@ -96,12 +116,11 @@ class _ProfileState extends State<Profile>{
                           return null;
                         },
                         decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0)
-                            ),
+                          contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                            border: OutlineInputBorder(),
                             hintText: 'Family',
                             hintStyle: TextStyle(
-                              color: Colors.black,
+                              color: Colors.grey,
                               fontWeight: FontWeight.bold,
                               fontSize: 12,
                             )
@@ -111,16 +130,16 @@ class _ProfileState extends State<Profile>{
 
 
                     Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                      child: TextField(
 
                           decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0)
-                              ),
+                              contentPadding: EdgeInsets.symmetric(horizontal: 15),
+
+                              border: OutlineInputBorder(),
                               hintText: 'Address',
                               hintStyle: TextStyle(
-                                  color: Colors.black,
+                                  color: Colors.grey,
                                   fontWeight: FontWeight.bold,
                                   fontSize : 12
                               )
@@ -129,15 +148,24 @@ class _ProfileState extends State<Profile>{
                     ),
 
                   Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                     child: FormField<String>(
                       builder: (FormFieldState<String> state) {
                         return InputDecorator(
                           decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0))),
+                              contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                              border: OutlineInputBorder()),
                           child: DropdownButtonHideUnderline(
+
                             child: DropdownButton<String>(
-                              hint: Text("Select One"),
+                              hint: Text("Select One",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold
+                              ),
+                              ),
+
                               value: currentSelectedValue,
                               isDense: true,
                               onChanged: (newValue) {
@@ -159,66 +187,172 @@ class _ProfileState extends State<Profile>{
                     ),
                   ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Travelling for work?',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Travelling for work?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14
+                          ),
                         ),
-                      ),
 
-                      Switch(
-                        value: _isSwitch,
-                        activeColor: Colors.blue,
-                        activeTrackColor: Colors.lightBlueAccent,
-                        onChanged: (value){
-                          setState(() {
-                            _isSwitch = value;
-                          });
-                        },
-                      )
-                    ],
-                  ),
-
-
-                  SizedBox(
-                    width: double.infinity, // match parent
-                  child: RaisedButton(
-
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        ),
-                    color: Colors.red,
-                    onPressed: (){
-
-                      if(_formKey.currentState.validate()){
-                        Scaffold.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                  'با موفقیت ثبت شد'
-                              ),
-                            )
-                        );
-                      }
-                    },
-                    child: Text(
-                      'Confirm',
-                      style: TextStyle(
-                        color: Colors.white
-                      ),
+                        Switch(
+                          value: _isSwitch,
+                          activeColor: Colors.blue,
+                          activeTrackColor: Colors.lightBlueAccent,
+                          onChanged: (value){
+                            setState(() {
+                              _isSwitch = value;
+                            });
+                          },
+                        )
+                      ],
                     ),
                   ),
+
+                  const Divider(
+                    color: Colors.grey,
+                    height: 4,
+                  ),
+
+
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child:Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+
+                        children: [
+                          Text(
+                            'Delivery',
+                            style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+
+                  Container(
+                    height: 200,
+                  child: ListView(
+
+                    children: _checkBoxValues.keys.map((String key) {
+                      return new CheckboxListTile(
+                        activeColor: Colors.blue,
+
+                        title: new Text(key,
+                            style: ThemeText().TextTheme1()
+                        ),
+                        value: _checkBoxValues[key],
+                        onChanged: (bool value) {
+                          setState(() {
+                            _checkBoxValues[key] = value;
+                          });
+                        },
+                      );
+                    }).toList()
+                  ),
+                  ),
+
+
+                  const Divider(
+                    color: Colors.grey,
+                    height: 4,
+                  ),
+
+
+                  Container(
+                    height: 200,
+                    child: ListView(
+
+                        children: _checkBoxValues.keys.map((String key) {
+                          return new CheckboxListTile(
+                            activeColor: Colors.blue,
+
+                            title: new Text(key,
+                                style: ThemeText().TextTheme1()
+                            ),
+                            value: _checkBoxValues[key],
+                            onChanged: (bool value) {
+                              setState(() {
+                                _checkBoxValues[key] = value;
+                              });
+                            },
+                          );
+                        }).toList()
+                    ),
+                  ),
+
+                  Column(
+                    children: <Widget>[
+
+//                      RadioGroup<String>.builder(
+//                        groupValue: _verticalGroupValue,
+//                        onChanged: (value) => setState(() {
+//                          _verticalGroupValue = value;
+//                        }),
+//                        items: _status,
+//                        itemBuilder: (item) => RadioButtonBuilder(
+//                          item,
+//                        ),
+//                      ),
+//                      RadioGroup<String>.builder(
+//                        groupValue: _verticalGroupValue,
+//                        onChanged: (value) => setState(() {
+//                          _verticalGroupValue = value;
+//                        }),
+//                        items: _status,
+//                        itemBuilder: (item) => RadioButtonBuilder(
+//                          item,
+//                          textPosition: RadioButtonTextPosition.left,
+//                        ),
+//                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: SizedBox(
+                      width: double.infinity, // match parent
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          ),
+                      color: Colors.red,
+                      onPressed: (){
+
+                        if(_formKey.currentState.validate()){
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'با موفقیت ثبت شد'
+                                ),
+                              )
+                          );
+                        }
+                      },
+                      child: Text(
+                        'Confirm',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                    ),
                   )
 
                 ],
               ),
             ),
-    ),
+          ),
+        ]
+      )
     )
-    );
-
+  );
   }
 }
---
+++ lib/src/utils/text_styles.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+ class ThemeText {
+  TextStyle TextTheme1() {
+    return TextStyle(
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.normal,
+        fontSize: 14);
+  }
+}
